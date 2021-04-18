08bebb224bc37f8e15409909bcad005ab45a0db9
Travis Smith <travis.a.smith.ja@gmail.com>
2021-04-17 21:15:18 -0500

    Adds loging for signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,30 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Logger, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dto/signup.dto';
 
 @Controller('auth')
 export class AuthController {
+  private logger = new Logger('AuthController');
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
   async signup(
     @Body(ValidationPipe) signupDto: SignupDto,
   ): Promise<{ accessToken: string }> {
-    return await this.authService.signup(signupDto);
+    this.logger.verbose(
+      `Sign up request for ${JSON.stringify({
+        emailAddress: signupDto.emailAddress,
+        name: signupDto.name,
+      })}`,
+    );
+    const authServiceResponse = await this.authService.signup(signupDto);
+
+    this.logger.verbose(
+      `Successful signup from ${JSON.stringify({
+        emailAddress: signupDto.emailAddress,
+        name: signupDto.name,
+      })}`,
+    );
+    return authServiceResponse;
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,12 +4,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
+import * as config from 'config';
+
+const jwtConfig = config.get('jwt');
+const jwtSecret = process.env.JWT_SECRET || jwtConfig.secret;
+const jwtExpiresIn = process.env.JWT_EXPIRES_IN || jwtConfig.expiresIn;
 
 @Module({
   imports: [
     JwtModule.register({
-      secret: 'secret',
-      signOptions: { expiresIn: 360000 },
+      secret: jwtSecret,
+      signOptions: { expiresIn: jwtExpiresIn },
     }),
     TypeOrmModule.forFeature([UserRepository]),
   ],
